ccd6fe2707116191a50e045731e5dca31762c4f0
still-a-beginner <81685698+still-a-beginner@users.noreply.github.com>
2021-05-11 22:30:02 +0530

    commit

--- linkedlist.py
+++ linkedlist.py
@@ -2,13 +2,13 @@
 #Insert/Remove item at the beginning: linkedlist Θ(1) time complexity. Use linkedlists.
 #Insert/Remove item at the end or any other point: linkedlist Θ(len(l)) time complexity Use arrays.
 
-#The node consists of a a constructor node which has a data and reference initially.
+#The node object consists of a constructor node which has a data and reference initially.
 class Node(object):
     def __init__(self,data):
         self.data = data
         self.nextNode = None
 
-#The linkedlist object which has a reference head and size initially
+#The linkedlist object has a constructor which has a reference head and size initially
 class LinkedList(object):
     def __init__(self):
         self.size = 0
@@ -20,6 +20,7 @@ class LinkedList(object):
         self.size = self.size + 1
         newNode = Node(data)
         
+        #Reference f the node is made below.
         #If the linkedlist is initially empty then the there is no next node. So make a node to refer to the null or none value.
         if not self.head:
             self.head = newNode
