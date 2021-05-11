505f18a82cac041a47fa47801a8098805541d12c
still-a-beginner <81685698+still-a-beginner@users.noreply.github.com>
2021-05-11 18:27:15 +0530

    commit

--- arrays.py
+++ arrays.py
@@ -1,8 +1,8 @@
 #The add function is to push the number, n at the end of list, l. The time complexity is Θ(1).
-def add(l, n):
+def add(l,n):
     print('\nAdd function')
-    print('Initial list is', l)
-    print('Number to be added is', n)
+    print('Initial list is',l)
+    print('Number to be added is',n)
     l.append(n)
     print('The final list is')
     return l
@@ -10,8 +10,8 @@ def add(l, n):
 #The remove function is to pop the number from the end of the list, l. The time complexity is Θ(1).
 def remove(l):
     print('\nRemove function')
-    print('Initial list is', l)
-    print('The number to be removed is', l[-1])
+    print('Initial list is',l)
+    print('The number to be removed is',l[-1])
     l = l[:-1]
     return l
 
@@ -19,8 +19,8 @@ def remove(l):
 #Use other data structure to insert an element at a particular index.
 def insert(l,i,n):
     print('\nInsert function')
-    print('Initial list is', l)
-    print('Number to be added is', n, 'at an index of', i)
+    print('Initial list is',l)
+    print('Number to be added is',n, 'at an index of',i)
     l1 = l[:i]
     l2 = l[i:]
     l1.append(n)
@@ -31,14 +31,28 @@ def insert(l,i,n):
 #Use other data structure to delete an element at a particular index.
 def delete(l,i):
     print('\nDelete function')
-    print('Initial list is', l)
-    print('Number to be deleted is at an index of', i)
+    print('Initial list is',l)
+    print('Number to be deleted is at an index of',i)
     l1 = l[:i]
     l2 = l[i+1:]
     l = l1 + l2
     return l
 
+#The retrieve function is to get the number at a particular index, i in the list, l. The time complexity is Θ(1).
+def retrieve(l,i):
+    print('\nRetrieve function')
+    print('Initial list is',l)
+    print('Number to be retrieved is at an index of',i)
+    return l[i]
 
+#The update function is to change a number at a particular index, i in the list, l by the number, n. The time complexity is Θ(1).
+def update(l,i,n):
+    print('\nUpdate function')
+    print('Initial list is',l)
+    print('Number to be updated is at an index of',i)
+    l[i] = n
+    return l
+    
 n = 8
 i = 2
 
@@ -56,4 +70,12 @@ print(l)
 
 l = [1,2,3,4]
 l = delete(l,i)
+print(l)
+
+l = [1,2,3,4]
+l = retrieve(l,i)
+print(l)
+
+l = [1,2,3,4]
+l = update(l,i,n)
 print(l)
