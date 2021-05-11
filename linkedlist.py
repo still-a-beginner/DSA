aa9996927ba03cc9aba6d69ba7fb3a248496eb4d
still-a-beginner <81685698+still-a-beginner@users.noreply.github.com>
2021-05-11 21:58:21 +0530

    commit

--- linkedlist.py
+++ linkedlist.py
@@ -1,3 +1,34 @@
 #The linkedlist is useful in many places. It has  afew disadvantages and advantages over lists
 #Insert/Remove item at the beginning: linkedlist Θ(1) time complexity. Use linkedlists.
 #Insert/Remove item at the end or any other point: linkedlist Θ(len(l)) time complexity Use arrays.
+
+#The node consists of a a constructor node which has a data and reference initially.
+class Node(object):
+    def __init__(self,data):
+        self.data = data
+        self.nextNode = None
+
+#The linkedlist object which has a reference head and size initially
+class LinkedList(object):
+    def __init__(self):
+        self.size = 0
+        self.head = None
+    
+    #Use linkedlist to insert data at the start of the object. It has time complexty of Θ(1).
+    #A new node of Node object type with data is made in inserting in linkedlist.
+    def insertStart(self,data):
+        self.size = self.size + 1
+        newNode = Node(data)
+        
+        #If the linkedlist is initially empty then the there is no next node. So make a node to refer to the null or none value.
+        if not self.head:
+            self.head = newNode
+        
+        #Else the linkedlist is non empty then the 
+        else:
+            newNode.nextNode = self.head
+            self.head = newNode
+    
+    #The size of the linkedlist is returned
+    def size(self):
+        return self.size
