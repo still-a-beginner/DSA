8b1117ed0c384f5dd9449d17636b4e37d7bec523
still-a-beginner <81685698+still-a-beginner@users.noreply.github.com>
2021-05-11 19:08:07 +0530

    commit

---
+++ linkedlist.py
@@ -0,0 +1,3 @@
+#The linkedlist is useful in many places. It has  afew disadvantages and advantages over lists
+#Insert/Remove item at the beginning: linkedlist Θ(1) time complexity. Use linkedlists.
+#Insert/Remove item at the end or any other point: linkedlist Θ(len(l)) time complexity Use arrays.
