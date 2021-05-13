969c2c8bf794304fb3a797d275afee4546431286
still-a-beginner <81685698+still-a-beginner@users.noreply.github.com>
2021-05-13 09:21:06 +0530

    commit

--- stack.py
+++ stack.py
@@ -1,4 +1,7 @@
-#Stack follows LIFO : Last in First out method.
+#Stack follows LIFO : Last in First out method. It has limited space unlike Heaps. It is faster than Heaps. 
+#The space in CPU is not fragmented unlike Heaps where memory is fragmented. 
+#Resizing of variables can not take place in Stacks but in Heaps, there can be resizing which can take place.
+#Recursion takes place in stacks in memory.
 
 #Stack object has a constructor which is an array/list itself.
 class Stack(object):
@@ -10,17 +13,17 @@ class Stack(object):
     def isEmpty(self):
         return self.stack == []
     
-    #Push is to append in the list
+    #Push is to append in the list. It has time complexity as Θ(1).
     def push(self,data):
         self.stack.append(data)
         
-    #Pop is to remove the top most or most recently added item.
+    #Pop is to remove the top most or most recently added item. It has time complexity as Θ(1).
     def pop(self):
         data = self.stack[-1]
         del self.stack[-1]
         return data
     
-    #Peek is to give the top most item in a stack.
+    #Peek is to give the top most item in a stack. It has time complexity as Θ(1).
     def peek(self):
         return self.stack[-1]
         
